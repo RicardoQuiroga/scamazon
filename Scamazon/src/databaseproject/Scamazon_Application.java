e9618f33e40a1e1fc92634d79bff829ff4172921
Jacob Corey <44644055+coreyja1@users.noreply.github.com>
2020-04-16 21:30:14 -0400

    Finished the users class and added user creation to the main class
    I implemented all of the features except the images that were in the readme in the users class.

--- Scamazon/src/databaseproject/Scamazon_Application.java
+++ Scamazon/src/databaseproject/Scamazon_Application.java
@@ -18,21 +18,15 @@ public class Scamazon_Application {
 				System.out.println("Welcome to Scamazon!");
 				System.out.println("Select an Option:");
 				System.out.println("1: Log in");
-				System.out.println("2: Reset Password");
-				System.out.println("3: Create Account");
-				System.out.println("4: Exit\n");
+				System.out.println("2: Create Account");
+				System.out.println("3: Exit\n");
 				String input = user.nextLine();
-				
+
 				if (input.equals("1")) {
 					logIn(myCon, user);
-				}
-				else if (input.equals("2")) {
-					resetPassword(myCon, user);
-				}
-				else if (input.equals("3")) {
+				} else if (input.equals("2")) {
 					createAccount(myCon, user);
-				}
-				else if (input.equals("4")) {
+				} else if (input.equals("3")) {
 					loop = 0;
 				} else {
 					System.out.println("Please enter a valid input.\n");
@@ -44,65 +38,132 @@ public class Scamazon_Application {
 			System.out.println(e);
 		}
 	}
-	
+
 	public static void logIn(Connection myCon, Scanner user) throws SQLException {
 		try {
 			boolean valid = false;
-			//will first determine if user is customer or employee. Will allow different options for different people
+
+			// will first determine if user is customer or employee. Will allow different
+			// options for different people
 			System.out.print("Enter Username or Employee ID:  ");
 			String userID = user.nextLine();
 			System.out.println();
 			System.out.print("Enter Password:  ");
 			String password = user.nextLine();
 			System.out.println();
-			
-			//prevents SQLInjection from login attempts
+
+			// prevents SQLInjection from login attempts
 			PreparedStatement login = myCon.prepareStatement("Select * from users where userid=? and userpass=?");
 			login.setString(1, userID.trim());
 			login.setString(2, password.trim());
 			ResultSet userLogin = login.executeQuery();
-			
-			//if user exists create users class
+
+			// if user exists create users class
 			if (userLogin.next()) {
-				valid = true;
-				System.out.println("Welcome to Scamazon " + userID +"\n");
-				Scamazon_Users.customerOptions(myCon, user, userID);
+				if (userLogin.getString(2).equals(userID) && userLogin.getString(4).equals(password)) {
+					valid = true;
+					System.out.println("Welcome to Scamazon " + userID + "!\n");
+					Scamazon_Users.customerOptions(myCon, user, userID);
+				}
 			}
 
 			login = myCon.prepareStatement("Select * from emp where empid=? and empPass=?");
 			login.setString(1, userID.trim());
 			login.setString(2, password.trim());
 			userLogin = login.executeQuery();
-			
-			//if employee exists create emp class
+
+			// if employee exists create emp class
 			if (userLogin.next()) {
-				valid = true;
-				Scamazon_Emp.employeeOptions(myCon, user, userID);
+				if (userLogin.getString(1).equals(userID) && userLogin.getString(2).equals(password)) {
+					valid = true;
+					Scamazon_Emp.employeeOptions(myCon, user, userID);
+				}
 			}
-			
+
 			if (valid == false) {
 				System.out.println("Either the Username doesn't exist or the Password was incorrect.\n");
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	
-	
-	public static void resetPassword(Connection myCon, Scanner user) throws SQLException {
-		try {
-			
-		}catch (Exception e) {
-			System.out.println(e);
-		}
-	}
-	
+
 	public static void createAccount(Connection myCon, Scanner user) throws SQLException {
+		String p1 = null;
+		String userID = null;
+		String email = null;
+		String name = null;
+		String address = null;
+		String city = null;
+		String post = null;
+		String state = null;
 		try {
-			
-		}catch (Exception e) {
-			System.out.println(e);
+			int loop = 1;
+			int restart = 0;
+
+			do {
+				restart = 0;
+				System.out.println("Creating account.\n");
+				System.out.print("Enter your user name: ");
+				userID = user.nextLine();
+				System.out.print("Enter your email address: ");
+				email = user.nextLine();
+				while (true) {
+					System.out.print("Enter your password: ");
+					p1 = user.nextLine();
+					System.out.print("Re-enter your password: ");
+					String p2 = user.nextLine();
+					if (p1.equals(p2)) {
+						break;
+					} else {
+						System.out.println("The passwords do not match.\n");
+					}
+				}
+				System.out.print("Enter your First and Last name: ");
+				name = user.nextLine();
+				System.out.print("Enter the Address:  ");
+				address = user.nextLine();
+				System.out.print("Enter your City: ");
+				city = user.nextLine();
+				System.out.print("Enter your Postal Code:  ");
+				post = user.nextLine();
+				System.out.print("Enter your State:  ");
+				state = user.nextLine();
+
+				// testing for complete info
+
+				String data[] = { userID, email, p1, name, address, city, post, state };
+				for (int i = 0; i < data.length; i++) {
+					if (data[i].isEmpty()) {
+						restart = 1;
+					}
+				}
+				if (restart == 1) {
+					System.out.println("Restarting, please enter all required information.");
+				} else {
+					loop = 0;
+				}
+			} while (loop == 1);
+
+			Statement stat1 = myCon.createStatement();
+			ResultSet id = stat1.executeQuery(("Select userID from users " + "where userID = " + '"' + userID + '"'));
+			id.next();
+			id.getString(1);
+			System.out.println("This account already exists.\n");
+
+		} catch (Exception e) {
+			Statement stat1 = myCon.createStatement();
+			stat1.execute("start transaction");
+			PreparedStatement prep1 = myCon.prepareStatement("insert into users "
+					+ "values ((select max(u.userNum) + 1 from users u),?, ?, ?, ?, ?, ?, ?, ?, 1)");
+			String data[] = { userID, email, p1, name, address, city, post, state };
+			for (int i = 0; i < data.length; i++) {
+				prep1.setString((i + 1), data[i]);
+			}
+			prep1.executeUpdate();
+			stat1.execute("commit");
+			System.out.println();
+			System.out.println("Your account has been created. Welcome to Scamazon!\n");
 		}
 	}
 }
--- Scamazon/src/databaseproject/Scamazon_Users.java
+++ Scamazon/src/databaseproject/Scamazon_Users.java
@@ -25,8 +25,9 @@ public class Scamazon_Users {
 				System.out.println("1: View Products");
 				System.out.println("2: View Cart");
 				System.out.println("3: Checkout");
-				System.out.println("4: Edit User Information");
-				System.out.println("5: Exit\n");
+				System.out.println("4: Add a Review");
+				System.out.println("5: Change Password");
+				System.out.println("6: Exit\n");
 				String input = user.nextLine();
 
 				if (input.equals("1")) {
@@ -35,13 +36,18 @@ public class Scamazon_Users {
 					viewCart();
 				} else if (input.equals("3")) {
 					checkout(myCon, user, userID);
+				} else if (input.equals("4")) {
+					addReview(myCon, user, userID);
 				} else if (input.equals("5")) {
+					resetPassword(myCon, user, userID);
+				} else if (input.equals("6")) {
 					loop = 0;
 				} else {
 					System.out.println("Please enter a valid input.\n");
 				}
 			}
-			System.out.println("Now exiting. Thank you for choosing Scamazon");
+			System.out.println("Now exiting. Thank you for choosing Scamazon\n");
+			cart.clear();
 
 		} catch (Exception e) {
 			System.out.println(e);
@@ -57,7 +63,7 @@ public class Scamazon_Users {
 				int count = 1;
 				Statement stat1 = myCon.createStatement();
 				ResultSet products = stat1.executeQuery("select description from categories");
-				System.out.println("Select a Category of Product: (1-4)");
+				System.out.println("Select a Category of Product: (1-4), Enter 0 to return to the main menu.");
 				while (products.next()) {
 					System.out.println(count + ": " + products.getString(1));
 					count++;
@@ -65,14 +71,19 @@ public class Scamazon_Users {
 				System.out.println();
 				String input = user.nextLine();
 				ResultSet items = stat1.executeQuery(
-						"select itemID, itemName, itemPrice, itemStock, description from items where itemcategory = "
+						"select itemID, itemName, itemPrice, itemStock, description, avgRating from items where itemcategory = "
 								+ "'" + input + "'");
 				System.out.println();
 
-				if (items.next() == true) {
+				if (input.equals("0")) {
+					loop = false;
+				}
+
+				else if (items.next() == true) {
 
 					System.out.println("Product ID: " + items.getString(1) + ", " + items.getString(2) + ", Price:"
-							+ items.getString(3) + ", Supply:" + items.getString(4));
+							+ items.getString(3) + ", Supply:" + items.getString(4) + ", Rating: "
+							+ items.getString(6));
 					System.out.println(items.getString(5));
 					System.out.println("---------------------------------------\n");
 
@@ -80,25 +91,28 @@ public class Scamazon_Users {
 						System.out.print("Product ID: " + items.getString(1) + ", " + items.getString(2) + ", Price: "
 								+ items.getString(3) + ", ");
 						if (Integer.parseInt(items.getString(4)) >= 5) {
-							System.out.print("Supply: in stock\n");
+							System.out.print("Supply: in stock, Rating: " + items.getString(6) + "\n");
 						} else if (Integer.parseInt(items.getString(4)) == 0) {
 							System.out.print("This item is out of stock\n");
 						} else if (Integer.parseInt(items.getString(4)) < 5) {
-							System.out.print("Supply: " + items.getString(4) + "\n");
+							System.out.print("Supply: " + items.getString(4) + ",Rating: " + items.getString(6) + "\n");
 						}
 						System.out.println(items.getString(5));
 						System.out.println("---------------------------------------\n");
 					}
+					System.out.println(
+							"Enter 1 to add items to cart. Enter 2 to view reviews. Enter 0 to exit. Any other key to return to product select.");
+					input = user.nextLine();
+					if (input.equals("1")) {
+						addItem(user, myCon);
+					} else if (input.equals("2")) {
+						viewReviews(myCon, user);
+					} else if (input.equals("0")) {
+						loop = false;
+					}
+
 				} else {
 					System.out.println("This category does not contain any items at this time.\n");
-				}
-				System.out.println(
-						"Enter 1 to add items to cart. Enter 0 to exit. Any other key to return to product select.");
-				input = user.nextLine();
-				if (input.equals("1")) {
-					addItem(user, myCon);
-				} else if (input.equals("0")) {
-					loop = false;
 				}
 			}
 		} catch (Exception e) {
@@ -107,32 +121,41 @@ public class Scamazon_Users {
 	}
 
 	public static void addItem(Scanner user, Connection myCon) throws SQLException {
-
-		boolean loop = true;
-		do {
-			System.out.print("Enter the Product ID to be added: ");
-			String input = user.nextLine();
-			Statement stat1 = myCon.createStatement();
-			ResultSet r1 = stat1.executeQuery(
-					"select itemid, itemStock, itemName, itemPrice from items where itemid = " + "'" + input + "'");
-			if (r1.next() && Integer.parseInt(r1.getString(2)) != 0) {
-				String items[] = { input, r1.getString(3), r1.getString(4) };
-				cart.add(items);
-				if (cart.size() == 1) {
-					System.out.println("This item has been added! There is " + cart.size() + " item in your cart.");
+		try {
+			boolean loop = true;
+			do {
+				System.out.print("Enter the Product ID to be added: ");
+				String input = user.nextLine();
+				System.out.print("Enter the quantity to be added: ");
+				String in2 = user.nextLine();
+				Statement stat1 = myCon.createStatement();
+				ResultSet r1 = stat1.executeQuery(
+						"select itemid, itemStock, itemName, itemPrice from items where itemid = " + "'" + input + "'");
+				if (r1.next() && Integer.parseInt(r1.getString(2)) != 0
+						&& Integer.parseInt(r1.getString(2)) >= Integer.parseInt(in2)) {
+					String items[] = { input, r1.getString(3), r1.getString(4), in2 };
+					cart.add(items);
+					if (cart.size() == 1) {
+						System.out
+								.println("This item has been added! There is " + cart.size() + " item in your cart.\n");
+					} else {
+						System.out.println(
+								"This item has been added! There are " + cart.size() + " items in your cart.\n");
+					}
 				} else {
-					System.out.println("This item has been added! There are " + cart.size() + " items in your cart.");
+					System.out.println("This product could not be added.");
 				}
-			} else {
-				System.out.println("This product could not be added.");
-			}
-			System.out.println("Would you like to add another item to the cart? Enter yes or no.");
-			input = user.nextLine();
-			if (input.trim().equalsIgnoreCase("no")) {
-				loop = false;
+				System.out.println("Would you like to add another item to the cart? Enter yes or no.");
+				input = user.nextLine();
 				System.out.println();
-			}
-		} while (loop == true);
+				if (input.trim().equalsIgnoreCase("no")) {
+					loop = false;
+					System.out.println();
+				}
+			} while (loop == true);
+		} catch (Exception e) {
+			System.out.println("Please enter a valid quantity.\n");
+		}
 	}
 
 	public static void viewCart() {
@@ -142,7 +165,8 @@ public class Scamazon_Users {
 		} else {
 			System.out.println("Items in your cart.\n");
 			for (int i = 0; i < cart.size(); i++) {
-				System.out.println((i + 1) + ": " + cart.get(i)[1] + ", Price: " + cart.get(i)[2]);
+				System.out.println((i + 1) + ": " + cart.get(i)[1] + ", Price: " + cart.get(i)[2] + ", Quantity: "
+						+ cart.get(i)[3]);
 			}
 			System.out.println();
 		}
@@ -176,7 +200,7 @@ public class Scamazon_Users {
 					update.executeUpdate();
 					stat1.execute("commit");
 				} else if (input.trim().equalsIgnoreCase("yes")) {
-					// enter code here
+
 					stat1.execute("start transaction");
 					ResultSet userData = stat1
 							.executeQuery("Select userEmail from users where userid =" + "'" + userID + "'");
@@ -184,7 +208,20 @@ public class Scamazon_Users {
 					stat1.executeUpdate(
 							"insert into orders(userID, userEmail, orderedOn, shippedBy)" + " values(" + "'" + userID
 									+ "','" + userData.getString(1) + "', '" + java.time.LocalDate.now() + "', 'UPS')");
+
+					// update orderdetails
+
+					for (int i = 0; i < cart.size(); i++) {
+						stat1.executeUpdate("insert into orderDetails(orderID, itemID, quantity) "
+								+ "values((select max(orderID) from orders)" + ", '" + cart.get(i)[0] + "', '"
+								+ cart.get(i)[3] + "')");
+						stat1.executeUpdate("update items set itemStock = (itemStock - '" + cart.get(i)[3]
+								+ "') where itemID = '" + cart.get(i)[0] + "'");
+					}
 					stat1.execute("commit");
+
+					System.out.println("Your order has been placed!\n");
+					cart.clear();
 				} else {
 					System.out.println("Please enter yes or no.");
 				}
@@ -194,5 +231,116 @@ public class Scamazon_Users {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+	}
+
+	public static void addReview(Connection myCon, Scanner user, String userID) throws SQLException {
+		try {
+			System.out.println("Enter the productID to review.\n");
+			Statement stat1 = myCon.createStatement();
+			ResultSet rs = stat1
+					.executeQuery("select distinct d.itemID, i.itemName from orderDetails d, orders o, items i "
+							+ "where d.orderID = o.orderID and d.itemID = i.itemID and o.userID = '" + userID
+							+ "' group by d.itemID, i.itemName");
+			if (rs.next()) {
+				System.out.println(rs.getString(1) + ": " + rs.getString(2));
+
+				while (rs.next()) {
+					System.out.println(rs.getString(1) + ": " + rs.getString(2));
+				}
+				System.out.println();
+				String product = user.nextLine();
+				System.out.print("Enter a rating of 1-10: ");
+				String rating = user.nextLine();
+				System.out.println("Enter a review for the product.\n");
+				String review = user.nextLine();
+
+				if (Integer.parseInt(rating) >= 1) {
+					rs = stat1.executeQuery(
+							"select * from reviews where userID = '" + userID + "'" + "and itemID = '" + product + "'");
+					if (rs.next()) {
+						System.out.println(
+								"You have already placed a review for this item. Would you like to update the review? (yes or no)");
+						String input = user.nextLine();
+						System.out.println();
+						if (input.equalsIgnoreCase("yes")) {
+							stat1.execute("start transaction");
+							stat1.executeUpdate("update reviews set rating ='" + rating + "', review = '" + review
+									+ "' where itemID ='" + product + "' and userID = '" + userID + "'");
+							stat1.executeUpdate(
+									"update items set avgRating = (select avg(rating) from reviews where itemID = '"
+											+ product + "') " + "where itemID = '" + product + "'");
+							stat1.execute("commit");
+							System.out.println("Review has been updated!\n");
+						}
+					} else {
+						stat1.execute("start transaction");
+						stat1.executeUpdate("insert into reviews(itemID, rating, review, userID) " + "values('"
+								+ product + "','" + rating + "','" + review + "','" + userID + "')");
+						stat1.executeUpdate(
+								"update items set avgRating = (select avg(rating) from reviews where itemID = '"
+										+ product + "') " + "where itemID = '" + product + "'");
+						stat1.execute("commit");
+						System.out.println("Review has been placed!\n");
+					}
+				}
+			} else {
+				System.out.println("You have not placed any orders.\n");
+			}
+		} catch (Exception e) {
+			 System.out.println("Please enter a valid review score.\n");
+		}
+	}
+
+	public static void resetPassword(Connection myCon, Scanner user, String userID) throws SQLException {
+		Statement stat1 = myCon.createStatement();
+		System.out.print("Enter the new password: ");
+		String p1 = user.nextLine();
+		System.out.println();
+		System.out.print("Enter the password again to confirm change: ");
+		String p2 = user.nextLine();
+
+		if (p1.equals(p2)) {
+			stat1.execute("start transaction");
+			PreparedStatement prep = myCon
+					.prepareStatement("update users set userPass=? where userID = '" + userID + "'");
+			prep.setString(1, p1);
+			prep.executeUpdate();
+			stat1.execute("commit");
+			System.out.println();
+		} else {
+			System.out.println("The passwords were not the same\n");
+		}
+	}
+
+	public static void viewReviews(Connection myCon, Scanner user) throws SQLException {
+		boolean loop = true;
+		do {
+			System.out.print("Select the item to view reviews: ");
+			String product = user.nextLine();
+			PreparedStatement prep = myCon.prepareStatement(
+					"select r.review, i.itemName, r.rating, r.userID from reviews r, items i where r.itemID = ? and i.itemID = ?");
+			prep.setString(1, product);
+			prep.setString(2, product);
+			ResultSet rs = prep.executeQuery();
+			System.out.println();
+			if (rs.next()) {
+				System.out.println(rs.getString(2) + ":\n");
+				System.out.println(rs.getString(1) + " --- " + rs.getString(3) + "/10 --- " + rs.getString(4) + "\n");
+				System.out.println("---------------------------------------\n");
+				while (rs.next()) {
+					System.out
+							.println(rs.getString(1) + " --- " + rs.getString(3) + "/10 --- " + rs.getString(4) + "\n");
+					System.out.println("---------------------------------------\n");
+				}
+			} else {
+				System.out.println("This product has no reviews at this time\n");
+			}
+			System.out.println(
+					"Enter 0 to return to the product selection menu. Any other key to see another product's reviews");
+			String input = user.nextLine();
+			if (input.equals("0")) {
+				loop = false;
+			}
+		} while (loop == true);
 	}
 }
